fb689dd620c3ffbf8eeb85231eafa5b90b91f39e
Randi Waranugraha <randi.waranugraha@gmail.com>
2015-06-17 16:35:05 +0700

    minor fixes

--- app/src/main/java/com/mysentosa/android/sg/utils/HttpHelper.java
+++ app/src/main/java/com/mysentosa/android/sg/utils/HttpHelper.java
@@ -44,9 +44,14 @@ public class HttpHelper {
 			BASE_ADDRESSV2 = "http://sentosa-staging.2359media.net/APIv2/";
 			BASE_HOST = "http://sentosa-staging.2359media.net";
 		} else {
-			BASE_ADDRESS = "http://sentosa.2359media.net:8000/api/";
-			BASE_ADDRESSV2 = "http://sentosa.2359media.net:8000/APIv2/";
-			BASE_HOST = "http://sentosa.2359media.net:8000";
+//			BASE_ADDRESS = "http://sentosa.2359media.net:8000/api/";
+//			BASE_ADDRESSV2 = "http://sentosa.2359media.net:8000/APIv2/";
+//			BASE_HOST = "http://sentosa.2359media.net:8000";
+
+			//using staging server for a while
+			BASE_ADDRESS = "http://sentosa-staging.2359media.net/api/";
+			BASE_ADDRESSV2 = "http://sentosa-staging.2359media.net/APIv2/";
+			BASE_HOST = "http://sentosa-staging.2359media.net";
 		}
 	}
 
